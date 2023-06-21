7347aac5cf2787000e968c1464145c3c8225f194
iptvmix <122865211+iptvmix@users.noreply.github.com>
2023-06-21 17:07:44 +0200

    Update sozcutv.sh

--- sozcutv.sh
+++ sozcutv.sh
@@ -2,6 +2,6 @@
 # Thanks to pshanmu3 user on github
 echo $(dirname $0)
 cd $(dirname $0)
-python3 sozcutv.py > ../feed//main/sozcutv.m3u8
+python3 sozcutv.py > ../feed/blop/main/sozcutv.m3u8
 
 echo m3u grabbed
